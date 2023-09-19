32eec6007f49e9650d0702db317bae603798ceff
Crystal <crwainstock@gmail.com>
2023-09-19 12:45:55 +0200

    backend seems to be running, no routes yet, though

--- my-library-app/app.js
+++ my-library-app/app.js
@@ -2,13 +2,13 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
-const cors = require("cors");
 app.use(cors()); // add after 'app' is created
 
 app.use(logger("dev"));
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
