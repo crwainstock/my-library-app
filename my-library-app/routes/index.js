b5c18f05a60f8bb7b21a115ea85bd55f5287b084
Crystal <crwainstock@gmail.com>
2023-09-19 13:14:51 +0200

    add node-fetch

--- my-library-app/routes/index.js
+++ my-library-app/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../model/helper");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
