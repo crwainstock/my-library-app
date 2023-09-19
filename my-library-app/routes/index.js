ca8c1f116c1e0b26e3c7e8bb1bcd92b13d854c3a
Crystal <crwainstock@gmail.com>
2023-09-19 12:55:55 +0200

    backend html rendering in localhost:5000

--- my-library-app/routes/index.js
+++ my-library-app/routes/index.js
@@ -1,9 +1,11 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send({ title: "Express" });
+  res.send({ title: "My Library App" });
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
