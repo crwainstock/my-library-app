15bdc13a8b9084d214698afdea17b73800d42fea
Crystal <crwainstock@gmail.com>
2023-09-20 09:13:44 +0200

    added content to footer

---
+++ my-library-app/client/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <h3>
+      My Library was created with ❤️ by{" "}
+      <a href="https://github.com/crwainstock"> Crystal</a>
+    </h3>
+  );
+}
