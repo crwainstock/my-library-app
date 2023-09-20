497d4e39451e38daa20f861b89b09b34c69ceab4
Crystal <crwainstock@gmail.com>
2023-09-20 09:06:49 +0200

    started building routes in frontend, added notFound page

--- my-library-app/client/src/App.jsx
+++ my-library-app/client/src/App.jsx
@@ -1,6 +1,33 @@
-import { useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import Home from "./Pages/Home";
 import "./App.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route
+        path="mylibrary"
+        element={<MyLibrary />}
+        loader={booksLoader}
+        errorElement={<Error />}
+      />
+
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
 
 function App() {
   return <Home />;
