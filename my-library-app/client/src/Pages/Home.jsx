5f7caf2fe004611e398741ff16ddf602d22ad1c2
Crystal <crwainstock@gmail.com>
2023-09-21 11:39:57 +0200

    added note about useDataContext in home page

--- my-library-app/client/src/Pages/Home.jsx
+++ my-library-app/client/src/Pages/Home.jsx
@@ -4,7 +4,9 @@ import Loading from "../Components/Loading/Loading";
 import MyLibrary from "../Pages/MyLibrary";
 
 function Home() {
-  const { books, loading } = useDataContext();
+  // This is only needed if the book data will be accessed directly in this page. Currently,
+  // it's being rendered through the MyLibrary page, so the useDataContext isn't needed here.
+  // const { books, loading } = useDataContext();
   // console.log(books);
   return (
     <div className="App">
