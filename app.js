ce78b92d169d42db14e6990ae5460718bf2dfa24
Crystal <crwainstock@gmail.com>
2023-09-23 23:28:16 +0200

    update bookDetail button styling

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public"))); // This deploys the backend html file
 app.use(express.static(path.join(__dirname, "/client/dist"))); //Added per CodeOp instructions
 app.use(cors());
 
