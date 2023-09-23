e8d3abc0ceced737f2a36d81e8f49cf5001afb5d
Crystal <crwainstock@gmail.com>
2023-09-23 10:36:30 +0200

    delete mylibrary element classnames

--- my-library-app/client/src/Pages/MyLibrary.jsx
+++ my-library-app/client/src/Pages/MyLibrary.jsx
@@ -8,25 +8,21 @@ function MyLibrary() {
   return (
     <div className="my-library-container">
       {books.map((book) => (
-        <div
-          className="col-lg-4 col-md-6 col-12 ps-3 pe-3 mt-5"
-          id="book"
-          key={book.id + book.volumeInfo.title}
-        >
+        <div className="" id="book" key={book.id + book.volumeInfo.title}>
           <h5>{book.volumeInfo.title}</h5>
           <p>
             {book.volumeInfo?.authors?.[0]} {book.volumeInfo.authors?.[1]}{" "}
           </p>
           <img src={book.volumeInfo.imageLinks?.thumbnail} />
-          <div className="row mt-4">
+          <div className="">
             <Link to={`/myLibrary/${book.id}`}>
-              <button id="seeMore" className="btn btn-primary col">
+              <button id="seeMore" className="">
                 See More
               </button>
             </Link>
             <div id="deleteIcon" className="col" key={book.id}>
               <button
-                className="rounded btn btn-danger mt-2"
+                className=""
                 onClick={(e) => {
                   deleteBook(book.id);
                 }}
