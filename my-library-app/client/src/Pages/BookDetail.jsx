fd62fdc5511f70824e91fc8b43b0f8acc9bfed35
Crystal <crwainstock@gmail.com>
2023-09-23 11:33:41 +0200

    import useParams in bookdetail page

--- my-library-app/client/src/Pages/BookDetail.jsx
+++ my-library-app/client/src/Pages/BookDetail.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { useDataContext } from "../Hooks/useDataContext";
+import { useDataContext, useParams } from "../Hooks/useDataContext";
 import { Link } from "react-router-dom";
 
 function BookDetail() {
   const { books, loading } = useDataContext();
+
+  const params = useParams(); //A part of react-router
+  const ID = params.id; //Pulls the id from the react-router data to be used in the functions below --
+  // this bookId is also used in the URL for this page
 
   return (
     <div className="book-detail-container">
