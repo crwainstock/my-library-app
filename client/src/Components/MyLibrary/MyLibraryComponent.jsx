b976545fcbdabf36af4ecdbacc3a13575b660733
Crystal <crwainstock@gmail.com>
2023-09-25 09:24:22 +0200

    created mylibrary component to clean up mylibrary page

---
+++ client/src/Components/MyLibrary/MyLibraryComponent.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useDataContext } from "../../Hooks/useDataContext";
+import { Link } from "react-router-dom";
+import "./MyLibrary.css";
+
+export default function MyLibraryComponent() {
+  const { books, loading } = useDataContext();
+
+  return (
+    <div className="my-library-container">
+      {books.map((book) => (
+        <div
+          className="book-card"
+          id="book"
+          key={book.id + book.volumeInfo.title}
+        >
+          <h5>{book.volumeInfo.title}</h5>
+          <p>
+            {book.volumeInfo?.authors?.[0]} {book.volumeInfo.authors?.[1]}{" "}
+          </p>
+          <img src={book.volumeInfo.imageLinks?.thumbnail} />
+          <div className="button-container">
+            <Link to={`/myLibrary/${book.id}`}>
+              <button id="seeMore" className="">
+                See More
+              </button>
+            </Link>
+            <div id="deleteIcon" className="" key={book.id}>
+              <button
+                id="deleteBook"
+                className=""
+                onClick={(e) => {
+                  deleteBook(book.id);
+                }}
+              >
+                {" "}
+                Delete Book{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- client/src/Pages/MyLibrary.jsx
+++ client/src/Pages/MyLibrary.jsx
@@ -1,45 +1,15 @@
 import React from "react";
 import { useDataContext } from "../Hooks/useDataContext";
 import { Link } from "react-router-dom";
+import NavBar from "../Components/NavBar/NavBar";
+import MyLibraryComponent from "../Components/MyLibrary/MyLibraryComponent";
 import "./MyLibrary.css";
 
 function MyLibrary() {
-  const { books, loading } = useDataContext();
-  //   console.log(books);
   return (
-    <div className="my-library-container">
-      {books.map((book) => (
-        <div
-          className="book-card"
-          id="book"
-          key={book.id + book.volumeInfo.title}
-        >
-          <h5>{book.volumeInfo.title}</h5>
-          <p>
-            {book.volumeInfo?.authors?.[0]} {book.volumeInfo.authors?.[1]}{" "}
-          </p>
-          <img src={book.volumeInfo.imageLinks?.thumbnail} />
-          <div className="button-container">
-            <Link to={`/myLibrary/${book.id}`}>
-              <button id="seeMore" className="">
-                See More
-              </button>
-            </Link>
-            <div id="deleteIcon" className="" key={book.id}>
-              <button
-                id="deleteBook"
-                className=""
-                onClick={(e) => {
-                  deleteBook(book.id);
-                }}
-              >
-                {" "}
-                Delete Book{" "}
-              </button>
-            </div>
-          </div>
-        </div>
-      ))}
+    <div>
+      <NavBar />
+      <MyLibraryComponent />
     </div>
   );
 }
