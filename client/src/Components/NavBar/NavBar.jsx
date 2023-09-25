44e45f0d861d8d1c89e8ee1e289302741fa53417
Crystal <crwainstock@gmail.com>
2023-09-25 09:16:09 +0200

    created navbar component and css file

---
+++ client/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <div id="nav" className="navbar-container">
+      <Link to="/">
+        <button className="home-button">
+          <h5>Home</h5>
+        </button>
+      </Link>
+      <Link to="/myLibrary">
+        <button className="mylibrary-button">
+          <h5>My Library</h5>
+        </button>
+      </Link>
+    </div>
+  );
+}
