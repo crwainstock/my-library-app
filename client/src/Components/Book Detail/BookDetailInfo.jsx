b6044df58deab73d125b63d1d9a1e6bc1d484f74
Crystal <crwainstock@gmail.com>
2023-09-25 08:38:46 +0200

    create bookDetailInfo component to break up BookDetail page

---
+++ client/src/Components/Book Detail/BookDetailInfo.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useGetBookDetails } from "../Hooks/useGetBookDetails";
+
+function BookDetailInfo() {
+  const { book, loading, error, success, review, bookData } =
+    useGetBookDetails();
+
+  return (
+    <div id="bookDetails" className="">
+      {success ? (
+        <div id="success" className="">
+          <h3>Your review has been updated!</h3>
+        </div>
+      ) : (
+        <div></div>
+      )}
+      <div className="book-detail-info">
+        <div className="">
+          <img className="" src={book.volumeInfo?.imageLinks?.thumbnail} />
+          <h5>{book?.volumeInfo?.title}</h5>
+          <p>
+            {book.volumeInfo?.authors?.[0]} {book.volumeInfo?.authors?.[1]}{" "}
+          </p>
+        </div>
+        <div className="book-description-container">
+          <p>{book?.volumeInfo?.description.replace(/(<([^>]+)>)/gi, "")}</p>
+        </div>
+
+        {bookData.review ? (
+          <div className="review-container">
+            <p>{bookData.review}</p>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+}
