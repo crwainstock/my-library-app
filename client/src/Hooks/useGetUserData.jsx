9daf99d8f19de1e8df882e77e1cbf0c5eeea37cf
Crystal <crwainstock@gmail.com>
2023-10-04 19:46:17 +0200

    added login function to useGetUserData hook doc

--- client/src/Hooks/useGetUserData.jsx
+++ client/src/Hooks/useGetUserData.jsx
@@ -1,3 +1,37 @@
 import { useEffect, useState } from "react";
+import { useNavigate, Form } from "react-router-dom";
 
-export const useGetUserData = () => {};
+export const useGetUserData = () => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const login = async () => {
+    try {
+      let options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+      };
+      const result = await fetch("/users/login", options);
+      const data = await result.json();
+      if (!result.ok) setError(data.error);
+      else {
+        //store token locally
+        localStorage.setItem("token", data.token);
+        //redirect to private page
+        navigate("/mylibrary");
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
+};
