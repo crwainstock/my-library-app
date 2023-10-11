cdbc4f30a6f613934bb4c3d70335e804d9db4921
Crystal <crwainstock@gmail.com>
2023-10-11 08:26:15 +0200

    update data provider with multi-user hook data

--- client/src/Context/DataProvider.jsx
+++ client/src/Context/DataProvider.jsx
@@ -1,12 +1,18 @@
 import { createContext } from "react";
 import useGetLibraryData from "../Hooks/useGetLibraryData";
+import useGetLoginStatus from "../Hooks/useGetLoginStatus";
+import useGetUserLibrary from "../Hooks/useGetUserLibrary";
 
 export const DataContext = createContext();
 
 export function DataProvider({ children }) {
-  const { books, loading, error } = useGetLibraryData();
+  // These can be used in the non-auth version of the app
+  // const { books, loading, error } = useGetLibraryData();
+  // const value = { books, loading, error };
 
-  const value = { books, loading, error };
+  const { userBooks } = useGetUserLibrary();
+  const { isLoggedIn, userId } = useGetLoginStatus();
+  const value = { userBooks, isLoggedIn, userId };
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
 }
