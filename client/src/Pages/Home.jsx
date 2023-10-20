05935d710690636347d8e56e4edf25ccc5d1114f
Crystal <crwainstock@gmail.com>
2023-10-20 15:56:04 +0200

    update home page, added info content, not complete

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import { useDataContext } from "../Hooks/useDataContext";
 import Loading from "../Components/Loading/Loading";
 import NavBar from "../Components/NavBar/NavBar";
 import Search from "../Components/Search/Search";
+import "./home.css";
 
 function Home() {
   //This is needed so it knows whether the data is loading or not.
@@ -10,13 +11,18 @@ function Home() {
   // console.log(books);
   return (
     <div className="App">
-      <h1>My Library</h1>
       {loading == true && (
         <div className="loading">
           <Loading />
         </div>
       )}
-      <Search />
+      <div className="home-container">
+        <div className="home-info">
+          <h1>My Library</h1>
+          <p>Explore and collect your favorite children's books.</p>
+        </div>
+        <Search />
+      </div>
     </div>
   );
 }
