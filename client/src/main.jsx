8449093b16f60ae7639bdc8df1238d3f766a32b3
Crystal <crwainstock@gmail.com>
2023-10-21 12:25:46 +0200

    added tanstack libraries and client provider to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { DataProvider } from "./Context/DataProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <DataProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </DataProvider>
+  <QueryClientProvider client={queryClient}>
+    <DataProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </DataProvider>
+  </QueryClientProvider>
 );
