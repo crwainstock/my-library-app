03743cce679adcda0eec8e6ad6e8e974ae79f44a
Crystal <crwainstock@gmail.com>
2023-10-24 09:13:12 +0200

    clean up comments in useGetUserLibrary

--- client/src/Hooks/useGetUserLibrary.jsx
+++ client/src/Hooks/useGetUserLibrary.jsx
@@ -43,17 +43,13 @@ export const useGetUserLibrary = () => {
       //Get books from database for userId
       let results = await fetch(`users/userlibrary/${userId}`);
       let data = await results.json();
-      // console.log(data);
       let books = data.books;
-      // console.log(books); //returns array of books objects
       //Loop through books and search using bookId with the searchMyBooks function
       //Should return full book data from Google & set books as that data
       for (let i = 0; i < books.length; i++) {
-        //console.log(books[i].bookId); //Seems to be accessing the bookId here
         await searchUserBooksById(books[i].bookId); //Use search function to look up book details using bookId
-        // console.log(books[i].bookId);
       }
-      // console.log(userBooks);
+
       setLoading(false);
       return userBooks;
     } catch (error) {
